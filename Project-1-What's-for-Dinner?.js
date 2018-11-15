de9f2046e9b543608925fb7caa364c703a40381a
Bunnymisu <42974151+Bunnymisu@users.noreply.github.com>
2018-11-15 11:23:32 -0500

    Added the code for the "What's for Dinner?" Project

---
+++ Project-1-What's-for-Dinner?.js
@@ -0,0 +1,39 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+
+noStroke(); 
+stroke(194, 78, 78);
+
+//steak
+fill(110, 57, 0);
+strokeWeight(132);
+stroke(110, 57, 0);
+line(117,145,91,211);
+line(114,145,104,191);
+stroke(0, 0, 0);
+strokeWeight(9);
+arc(120,224,100,99,-9,42); //top half of steak
+arc(116,140,133,134,-144,9); //lower half
+arc(105,199,155,156,49,209);
+line(34,165,63,98); 
+line(169,214,182,151); //grill lines
+line(51,235,150,238);
+line(154,191,53,191);
+line(62,132,161,133); 
+
+//corn
+fill(255, 213, 0);
+ellipse(244,89,26,25);
+ellipse(272,90,26,25);
+ellipse(228,111,26,25);
+ellipse(255,117,26,25);
+ellipse(285,133,26,25);
+ellipse(274,117,26,25);
+ellipse(233,139,26,25);
+ellipse(263,146,26,25);
+ellipse(301,146,26,25);
+
+//potatos
+fill(255, 209, 209);
+ellipse(258,261,145,186);
